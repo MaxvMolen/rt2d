4f0f772b47d0111c3f06d0e36d8aa4a950ae9a3f
MaxvMolen <maxharm1811@gmail.com>
2017-12-13 14:20:36 +0100

    myscenebegin added this will be the main menu

---
+++ start/myscenebegin.cpp
@@ -0,0 +1,26 @@
+/**
+* This class describes MySceneBegin behavior.
+*
+* Copyright 2017 Max van der Molen <maxharm1811@gmail.com>
+*/
+
+#include <fstream>
+#include <sstream>
+#include "myscenebegin.h"
+
+
+MySceneBegin::MySceneBegin() : CoreScene()
+{
+	
+}
+
+
+MySceneBegin::~MySceneBegin()
+{
+
+}
+
+void MySceneBegin::update(float deltaTime)
+{
+	
+}
---
+++ start/myscenebegin.h
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2017 Max van der Molen <maxharm1811@gmail.com>
+ *
+ * @file myscenebegin.h
+ *
+ * @brief description of MySceneBegin behavior.
+ */
+
+#ifndef MYSCENEBEGIN_H
+#define MYSCENEBEGIN_H
+
+#include <rt2d/scene.h>
+#include "corescene.h"
+
+/// @brief The MyScene class is the Scene implementation.
+class MySceneBegin : public CoreScene
+{
+public:
+	/// @brief Constructor
+	MySceneBegin();
+	/// @brief Destructor
+	virtual ~MySceneBegin();
+
+	/// @brief update is automatically called every frame
+	/// @param deltaTime the elapsed time in seconds
+	/// @return void
+	virtual void update(float deltaTime);
+	
+protected:
+	
+private:
+	
+};
+
+#endif /* MYSCENEBEGIN_H */
