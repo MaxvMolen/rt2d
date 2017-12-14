7b22b4ff86613487eb1f04c9176cd70c3423a996
MaxvMolen <maxharm1811@gmail.com>
2017-12-14 11:13:44 +0100

    List of all scenes and cleanup at shutdown

--- start/main.cpp
+++ start/main.cpp
@@ -17,22 +17,51 @@
 #include <rt2d/core.h>
 
 #include "myscene.h"
-
+#include "myscenebegin.h"
 /// @brief main entry point
 int main( void )
 {
 	// Core instance
 	Core core;
 
+	std::vector<CoreScene*> myscenes;
+	myscenes.push_back(new MySceneBegin());
+	myscenes.push_back(new MyScene());
+	int s = myscenes.size();
+
 	// Scene01
+	CoreScene* scene = myscenes[0];
+	int scenecounter = 0;
+	int running = 1;
+	while (running) {
+		scenecounter = scene->activescene;
+		if (scenecounter > s-1) { scenecounter = 0; scene->activescene = 0; }
+		if (scenecounter < 0) { scenecounter = s-1; scene->activescene = s-1; }
+		scene = myscenes[scenecounter];
+		core.run(scene); // update and render the current scene
+		core.showFrameRate(5); // show framerate in output every n seconds
+		if (!scene->isRunning()) { running = 0; } // check status of Scene every frame
+	}
+
+	/*
 	MyScene* myscene = new MyScene(); // create Scene on the heap
 	while(myscene->isRunning()) { // check status of Scene every frame
 		core.run(myscene); // update and render the current scene
 		core.showFrameRate(5); // show framerate in output every n seconds
+	}*/
+
+	//delete all scenes
+	for (int i = 0; i < s; i++) {
+		delete myscenes[i];
+		myscenes[i] = NULL;
 	}
+	myscenes.clear();
+	
+	
+	/*
 	//core.cleanup(); // cleanup ResourceManager (Textures + Meshes, but not Shaders)
 	delete myscene; // delete Scene and everything in it from the heap to make space for next Scene
-
+	*/
 	// No need to explicitly clean up the core.
 	// As a local var, core will go out of scope and destroy Renderer->ResourceManager.
 	// ResourceManager destructor also deletes Shaders.
