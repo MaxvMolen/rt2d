8424070cda1f258cffd6ed8b08b1bc7bafdf090f
MaxvMolen <maxharm1811@gmail.com>
2018-01-11 10:54:08 +0100

    Switch scene is all items are removed and menu is false

--- start/myscene00.cpp
+++ start/myscene00.cpp
@@ -290,7 +290,7 @@ void MyScene00::update(float deltaTime)
 	// ###############################################################
 	// Change scene when all items are removed
 	// ###############################################################
-	if (mytree.size() == 0 && myperson.size() == 0 && mycar.size() == 0) {
+	if (mytree.size() == 0 && myperson.size() == 0 && mycar.size() == 0 && menu == false) {
 		CoreScene::sceneselect(2); // next level
 		started00 = false;
 	}
